b89d53c78bb8fdf417bcd8d32b45bb9b497a7a35
Razvan POP <razvan.pop@gmail.com>
2021-01-08 10:04:59 +0200

    oracle test

--- test/oracle/oracle.test.ts
+++ test/oracle/oracle.test.ts
@@ -9,52 +9,34 @@ import { deployContract } from 'ethereum-waffle';
 import { withCompoundRate, toWei, bondSlippage, toBNj, e18, ERROR_MARGIN_ACCEPTABLE } from '@testhelp/index';
 import { bbFixtures } from './../migrations';
 
+import OraclelizedMockArtifact from '../../artifacts/contracts/mocks/barnbridge/OraclelizedMock.sol/OraclelizedMock.json';
+import YieldOracleArtifact from './../../artifacts/contracts/lib/oracle/YieldOracle.sol/YieldOracle.json';
+
+import { YieldOracle } from '@typechain/YieldOracle';
+import { OraclelizedMock } from '@typechain/OraclelizedMock';
+
+const defaultWindowSize = 60 * 60 * 24 * 3;
+const defaultGranularity = 12 * 3; // samples in window
+
 const fixture = async (wallets: Wallet[]) => {
-  const [deployerSign, ownerSign, junior1Sign, junior2Sign, senior1Sign, senior2Sign] = wallets;
-  const [deployerAddr, ownerAddr, junior1Addr, junior2Addr, senior1Addr, senior2Addr] = await Promise.all([
+  const [deployerSign, ownerSign,] = wallets;
+  const [deployerAddr, ownerAddr,] = await Promise.all([
     deployerSign.getAddress(),
     ownerSign.getAddress(),
-    junior1Sign.getAddress(),
-    junior2Sign.getAddress(),
-    senior1Sign.getAddress(),
-    senior2Sign.getAddress(),
   ]);
 
-  const rewardCtoken = (await deployContract(deployerSign, Erc20MockArtefact, ['COMP', 'COMP'])) as Erc20Mock;
-  const underliying = (await deployContract(deployerSign, Erc20MockArtefact, ['DAI', 'DAI'])) as Erc20Mock;
-  const ctoken = (await deployContract(deployerSign, CTokenMockArtefact, [underliying.address])) as CTokenMock;
-
-  const juniorModel = (await deployContract(deployerSign, TokenPriceV1Artefact, [])) as TokenPriceV1;
-  const seniorModel = (await deployContract(deployerSign, SeniorBondSlippageV1Artefact, [])) as SeniorBondSlippageV1;
-
-  const pool = (await deployContract(deployerSign, SmartYieldPoolMockArtefact, [ctoken.address])) as SmartYieldPoolMock;
-
-  const juniorToken = (await deployContract(deployerSign, Erc20MockArtefact, ['jBOND', 'jBOND'])) as Erc20Mock;
-  const seniorToken = (await deployContract(deployerSign, SeniorBondTokenArtefact, ['sBOND', 'sBOND', pool.address])) as SeniorBondToken;
-
-  await pool.setCToken(ctoken.address);
-  await pool.setRewardCToken(rewardCtoken.address);
-  await pool.setUnderlying(underliying.address);
-
-  await pool.setJuniorModel(juniorModel.address);
-  await pool.setSeniorModel(seniorModel.address);
-
-  await pool.setJuniorToken(juniorToken.address);
-  await pool.setSeniorToken(seniorToken.address);
+  const oraclelizedMock = (await deployContract(deployerSign, OraclelizedMockArtifact, [])) as OraclelizedMock;
+  const yieldOracle = (await deployContract(deployerSign, YieldOracleArtifact, [oraclelizedMock.address, defaultWindowSize, defaultGranularity])) as YieldOracle;
 
   return {
-    rewardCtoken, underliying, ctoken, juniorModel, seniorModel, pool, juniorToken, seniorToken,
+    yieldOracle, oraclelizedMock,
     deployerSign: deployerSign as Signer,
     ownerSign: ownerSign as Signer,
-    junior1Sign: junior1Sign as Signer,
-    junior2Sign: junior2Sign as Signer,
-    senior1Sign: senior1Sign as Signer,
-    senior2Sign: senior2Sign as Signer,
-    deployerAddr, ownerAddr, junior1Addr, junior2Addr, senior1Addr, senior2Addr,
+    deployerAddr, ownerAddr,
   };
 };
 
-describe('sBond & jToken Prices', async function () {
+describe('Yield Oracle', async function () {
   it('Junior Token Price should be 1 at begining', async function () {
     const { pool } = await bbFixtures(fixture);
 
