72158e1048eb0d1adfdf0555d38f225cc67b2e29
Razvan Pop <razvan.pop@gmail.com>
2021-02-18 18:07:21 +0200

    - remove bondmodel comments for prev version eq - abiencoder v2 for bondmodel

--- contracts/model/BondModelV1.sol
+++ contracts/model/BondModelV1.sol
@@ -1,13 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.7.6;
-
-// x = (cur_j - (b_p*x*b_t)) / (cur_tot + b_p + (b_p*x*b_t)) * n
-// x = (cur_j - (bond*n*t*(cur_j/cur_tot))) / (cur_tot + bond + (bond*n*t*(cur_j/cur_tot))) * n;
-// n - oracle rate ( /day )
-// bond - new bond principal
-// t - bond duration (days)
-// cur_j - curent junior liquidity. totalUndwerlying() - abond.principal - abond.gain - (jtokens in withdrawl * jtoken price)
-// cur_tot - totalUnderlying()
+pragma abicoder v2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
@@ -30,7 +23,6 @@ contract BondModelV1 is IBondModel {
         uint256 total = ISmartYield(pool_).underlyingTotal();
         uint256 dailyRate = ISmartYield(pool_).providerRatePerDay();
 
-        //uint256 aproxGain = MathUtils.compound(principal, dailyRate * (loanable * 1e18 / (total + principal)) / 1e18, forDays).sub(principal);
         uint256 aproxGain = MathUtils.compound2(
           principal_,
           //dailyRate * (loanable * 1e18 / (total + principal)) / 1e18,
@@ -38,7 +30,6 @@ contract BondModelV1 is IBondModel {
           forDays_
         ).sub(principal_);
 
-        //uint256 rate = (loanable.sub(aproxGain, "BondModelV1: liquidity")) * 1e18 / (total + principal) * dailyRate / 1e18;
         uint256 rate = uint256(1e18).mul(dailyRate).mul(loanable.sub(aproxGain, "BondModelV1: liquidity")) / (total + principal_) / 1e18;
 
         return MathUtils.compound2(principal_, rate, forDays_).sub(principal_);
