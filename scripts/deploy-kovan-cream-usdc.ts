d5650c69b4405c8f78d58feecac558a90032e74a
Razvan Pop <razvan.pop@gmail.com>
2021-05-13 16:52:31 +0300

    kovan cream usdc deploy script

---
+++ scripts/deploy-kovan-cream-usdc.ts
@@ -0,0 +1,73 @@
+import 'tsconfig-paths/register';
+
+import { deployBondModelV2Compounded, deployCreamController, deployCreamProvider, deployJuniorBond, deploySeniorBond, deploySmartYield, deployYieldOracle } from '@testhelp/index';
+import { Wallet, BigNumber as BN } from 'ethers';
+import { run, ethers } from 'hardhat';
+import { ERC20Factory } from '@typechain/ERC20Factory';
+
+const A_HOUR = 60 * 60;
+
+const seniorBondCONF = { name: 'BarnBridge crUSDC sBOND', symbol: 'bb_sBOND_crUSDC' };
+const juniorBondCONF = { name: 'BarnBridge crUSDC jBOND', symbol: 'bb_jBOND_crUSDC' };
+const juniorTokenCONF = { name: 'BarnBridge crUSDC', symbol: 'bb_crUSDC' };
+
+const oracleCONF = { windowSize: A_HOUR, granularity: 4 };
+
+// barnbridge
+const decimals = 6; // same as USDC
+const dao = '0x930e52B96320d7dBbfb6be458e5EE0Cd3E5E5Dac';
+const feesOwner = dao;
+
+// externals ---
+
+// cream
+const crUSDC = '0x378630f9e1968Aa76b299636A837E737fa476037';
+
+const USDC = '0xb7a4f3e9097c08da09517b5ab877f7a917224ede';
+
+async function main() {
+
+  const [deployerSign, ...signers] = (await ethers.getSigners()) as unknown[] as Wallet[];
+
+  console.log('Deployer:', deployerSign.address);
+  console.log('Others:', signers.map(a => a.address));
+
+  const bondModel = await deployBondModelV2Compounded(deployerSign);
+  const pool = await deployCreamProvider(deployerSign, crUSDC);
+
+  const smartYield = await deploySmartYield(deployerSign, juniorTokenCONF.name, juniorTokenCONF.symbol, BN.from(decimals));
+  const seniorBond = await deploySeniorBond(deployerSign, smartYield.address, seniorBondCONF.name, seniorBondCONF.symbol);
+  const juniorBond = await deployJuniorBond(deployerSign, smartYield.address, juniorBondCONF.name, juniorBondCONF.symbol);
+
+  const controller = await deployCreamController(deployerSign, pool.address, smartYield.address, bondModel.address, deployerSign.address);
+  const oracle = await deployYieldOracle(deployerSign, controller.address, oracleCONF.windowSize, oracleCONF.granularity);
+
+  await controller.setOracle(oracle.address);
+  await controller.setFeesOwner(feesOwner);
+  await smartYield.setup(controller.address, pool.address, seniorBond.address, juniorBond.address);
+  await pool.setup(smartYield.address, controller.address);
+
+  await controller.setGuardian(dao);
+  await controller.setDao(dao);
+
+  console.log('CONF --------');
+  console.log('DAO:', dao);
+  console.log('crUSDC:', crUSDC);
+  console.log('USDC:', USDC);
+  console.log('');
+  console.log('DEPLOYED ----');
+  console.log('bondModel:', bondModel.address);
+  console.log('provider:', pool.address);
+  console.log('smartYield:', smartYield.address);
+  console.log('seniorBond:', seniorBond.address);
+  console.log('juniorBond:', juniorBond.address);
+  console.log('controller:', controller.address);
+  console.log('oracle:', oracle.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
