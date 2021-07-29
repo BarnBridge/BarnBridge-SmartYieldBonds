7b5eb2a7260999cbdebdefb04926f4480be8f032
Razvan Pop <razvan.pop@gmail.com>
2021-07-29 17:47:08 +0300

    fix bb_cUSDT deploy script

--- scripts/deploy-mainnet-compound-usdt.ts
+++ scripts/deploy-mainnet-compound-usdt.ts
@@ -1,6 +1,6 @@
 import 'tsconfig-paths/register';
 
-import { deployBondModel, deployCompoundController, deployCompoundProvider, deployJuniorBond, deploySeniorBond, deploySmartYield, deployYieldOracle } from '@testhelp/index';
+import { deployBondModel, deployBondModelV2Compounded, deployCompoundController, deployCompoundProvider, deployJuniorBond, deploySeniorBond, deploySmartYield, deployYieldOracle } from '@testhelp/index';
 import { Wallet, BigNumber as BN } from 'ethers';
 import { run, ethers } from 'hardhat';
 import { ERC20Factory } from '@typechain/ERC20Factory';
@@ -15,7 +15,7 @@ const juniorTokenCONF = { name: 'BarnBridge junior cUSDT', symbol: 'bb_cUSDT' };
 const oracleCONF = { windowSize: 3 * A_DAY, granularity: 4 };
 
 // barnbridge
-const decimals = 8; // same as USDT
+const decimals = 6; // same as USDT
 const feesOwner = '0x4cAE362D7F227e3d306f70ce4878E245563F3069';
 
 // externals ---
@@ -24,20 +24,18 @@ const feesOwner = '0x4cAE362D7F227e3d306f70ce4878E245563F3069';
 const cUSDT = '0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9';
 const COMP = '0xc00e94cb662c3520282e6f5717214004a7f26888';
 
-const USDT = '0xdac17f958d2ee523a2206206994597c13d831ec7';
-const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 const uniswapPath = [COMP, WETH, USDT];
 
 async function main() {
-
-  return;
 
   const [deployerSign, ...signers] = (await ethers.getSigners()) as unknown[] as Wallet[];
 
   console.log('Deployer:', deployerSign.address);
   console.log('Others:', signers.map(a => a.address));
 
-  const bondModel = await deployBondModel(deployerSign);
+  const bondModel = await deployBondModelV2Compounded(deployerSign);
   const pool = await deployCompoundProvider(deployerSign, cUSDT);
 
   const smartYield = await deploySmartYield(deployerSign, juniorTokenCONF.name, juniorTokenCONF.symbol, BN.from(decimals));
@@ -53,6 +51,7 @@ async function main() {
   await pool.setup(smartYield.address, controller.address);
 
   await controller.setBondLifeMax(30);
+  await bondModel.setMaxPoolRatio(BN.from('650000000000000000'));
 
   console.log('CONF --------');
   console.log('cUSDT:', cUSDT);
