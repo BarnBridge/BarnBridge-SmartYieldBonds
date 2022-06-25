3f35f6cde267100019f9dc86e4dd38e4778e5059
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-25 19:14:42 +0800

    Delete test directory

--- test/helpers/format.ts
+++
@@ -1,6 +0,0 @@
-import { BigNumber as BN } from 'ethers';
-import { BigNumber as BNj } from 'bignumber.js';
-
-export const withDecimals = (n: BN, decimals: number): string => {
-  return new BNj(n.toString()).dividedBy(10**decimals).toFixed(decimals);
-};
